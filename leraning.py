c302e25d1d7cf010ff8d1d31654e038595ea95c4
theprogrammer2025 <rainer_a@dell.com>
2024-10-22 11:39:27 -0500

    Create leraning.py
    this is a learning file

---
+++ leraning.py
@@ -0,0 +1,8 @@
+echo "# Learningrepository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/theprogrammer2025/Learningrepository.git
+git push -u origin main
+
